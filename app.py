ca295ca506fa79d3b8dbce79691ddd6e7a347c30
anjali10603 <ap5035355@gmail.com>
2026-05-22 19:44:57 +0530

    Refactor app.py

--- app.py
+++ app.py
@@ -1,64 +1,58 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 CORS(app)
 
-OPENROUTER_API_KEY = "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"
-print("KEY:", OPENROUTER_API_KEY)
+GROQ_API_KEY = "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"
 
 @app.route("/")
-
 def home():
-    return "Nisha Agro API Running ✅"
+    return send_from_directory('.', 'search.html')
 
 @app.route("/crop/<crop_name>")
-
 def crop(crop_name):
 
     prompt = f"""
     Give detailed farming information about {crop_name} crop.
 
     Include:
-    - Best soil
+    - Soil
     - Temperature
     - Water requirement
     - Fertilizers
     - Diseases
     - Harvesting
     - Profit
-    - Smart farming tips
+    - Smart result
     """
 
-    headers = {
-        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-        "Content-Type": "application/json"
-    }
-
-    data = {
-        "model": "mistralai/mistral-7b-instruct:free",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ]
-    }
-
     response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers=headers,
-        json=data
-    )
-    print(response.text)
+        "https://api.groq.com/openai/v1/chat/completions",
 
-    result = response.json()
+        headers={
+            "Authorization": f"Bearer {GROQ_API_KEY}",
+            "Content-Type": "application/json"
+        },
+
+        json={
+            "model": "llama3-8b-8192",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        }
+    )
+
+    data = response.json()
 
     try:
-        answer = result["choices"][0]["message"]["content"]
+        answer = data["choices"][0]["message"]["content"]
     except:
-        answer = str(result)
+        answer = str(data)
 
     return jsonify({
         "answer": answer
